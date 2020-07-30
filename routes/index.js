533b5ad731e3ccca2815a814a7f1c668a1c2952d
Claudio <cernandes.silva@gmail.com>
2020-07-30 11:27:48 -0300

    enviando contato com o método POST para o MySQL

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const conn = require('./../inc/db');
 const express = require('express');
 const menus = require('./../inc/menus');
 const reservations = require('./../inc/reservations');
-const { render } = require('./../inc/reservations');
+const contacts = require('./../inc/contacts');
 const router = express.Router();
 
 
@@ -18,11 +18,24 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/contacts', function (req, res, next) {
-  res.render('contacts', {
-    title: 'Contato - Restaurante Saboroso!',
-    background: 'images/img_bg_3.jpg',
-    h1: 'Diga um oi!'
-  });
+  contacts.render(req, res);
+});
+
+router.post('/contacts', function (req, res, next) {
+  if (!req.body.name) {
+    contacts.render(req, res, 'Digite o nome');
+  } else if (!req.body.email) {
+    contacts.render(req, res, 'Digite o email');
+  } else if (!req.body.message) {
+    contacts.render(req, res, 'Digite a menssagem');
+  } else {
+    contacts.save(req.body).then(results => {
+      req.body = {};
+      contacts.render(req, res, null, 'Contato enviado com sucesso!');
+    }).catch(err => {
+      contacts.render(req, res, err.message);
+    });
+  }
 });
 
 router.get('/menus', function (req, res, next) {
