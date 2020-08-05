b6ba1160fc979e0c5fb1c94bee0ef3e74374a315
Claudio <cernandes.silva@gmail.com>
2020-08-05 11:00:23 -0300

    administração reservations - criando nova reserva

--- inc/reservations.js
+++ inc/reservations.js
@@ -12,18 +12,35 @@ module.exports = {
     },
     save(fields) {
         return new Promise((resolve, reject) => {
-            let date = fields.date.split('/');
-            fields.date = `${date[2]}-${date[1]}-${date[0]}`
-            conn.query(`
-                INSERT INTO tb_reservations (name, email, people, date, time)
-                VALUES(?, ?, ?, ?, ?)
-                `, [
+            if (fields.date.indexof('/') > -1) {
+                let date = fields.date.split('/');
+                fields.date = `${date[2]}-${date[1]}-${date[0]}`
+            }
+            let query, params = [
                 fields.name,
                 fields.email,
                 fields.people,
                 fields.date,
                 fields.time
-            ], (err, results) => {
+            ];
+            if (parseInt(fields.id) > 0) {
+                query = `
+                    UPDATE tb_reservations
+                    SET name = ?,
+                        email = ?,
+                        people = ?,
+                        date = ?,
+                        time = ?
+                    WHERE id = ?
+                    `;
+                params.push(fields.id);
+            } else {
+                query = `
+                    INSERT INTO tb_reservations (name, email, people, date, time)
+                    VALUES(?, ?, ?, ?, ?)               
+                    `;
+            }
+            conn.query(query, params, (err, results) => {
                 console.log(err);
                 if (err) {
                     reject(err);
