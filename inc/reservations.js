a20c9405b6e1ab376714bd3c9e058e5a3af51590
Claudio <cernandes.silva@gmail.com>
2020-08-14 10:51:34 -0300

    paginação de dados, navegando entre páginas

--- inc/reservations.js
+++ inc/reservations.js
@@ -50,14 +50,28 @@ module.exports = {
             });
         });
     },
-    getReservations(page) {
-        if (!page) page = 1;
-        let pag = new Pagination(
-            `
-            SELECT SQL_CALC_FOUND_ROWS * FROM tb_reservations ORDER BY name LIMIT ?, ?
-            `
-        )
-        return pag.getPage(page);
+    getReservations(req) {
+        return new Promise((resolve, reject) => {
+            let page = req.query.page;
+            let dtstart = req.query.start;
+            let dtend = req.query.end;
+            if (!page) page = 1;
+            let params = []
+            if (dtstart && dtend) params.push(dtstart, dtend);
+            let pag = new Pagination(
+                `SELECT SQL_CALC_FOUND_ROWS * 
+                 FROM tb_reservations
+                 ${(dtstart && dtend) ? 'WHERE date BETWEEN ? AND ?' : ''}
+                 ORDER BY name LIMIT ?, ?
+                `, params
+            );
+            pag.getPage(page).then(data => {
+                resolve({
+                    data,
+                    links: pag.getNavigation(req.query)
+                });
+            });
+        });
     },
     delete(id) {
         return new Promise((resolve, reject) => {
